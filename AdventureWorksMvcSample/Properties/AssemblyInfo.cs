583e601940af762ef0e5f823371577f0a5692d75
sherman <sherman@xdev-ap.technology>
2015-11-11 21:51:12 +1100

    New Telerik ASP.NET MVC Sample Project
    New Telerik ASP.NET MVC Sample Project

---
+++ AdventureWorksMvcSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ( "AdventureWorksMvcSample" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "AdventureWorksMvcSample" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2015" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible ( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid ( "7c347f06-6d9c-4054-8fb3-c1df7ba8d6b4" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
